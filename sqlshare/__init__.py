96e48ab6939e74d79b80d2c80bfaff08211ed7fa
Garret Cole <gbc3@uw.edu>
2011-02-28 19:15:06 +0000

    refactored authentication protocol in __init__.py so that when new auth protocol is in place it can be updated in a single method instead of per function.
    modified the upload process to use a version2 of uploading files per chunk. This means the multistep process of uploading files is no longer needed. (see code)
    
    git-svn-id: https://subversion.assembla.com/svn/sqlshare/pythonclient@62 393f18f5-db89-43d5-bd78-748e0f63f621

--- sqlshare/__init__.py
+++ sqlshare/__init__.py
@@ -24,10 +24,12 @@ class SQLShareUploadError(SQLShareError):
   pass
 
 class SQLShare:
-  HOST = "sqlshare-rest.cloudapp.net" #"sqlshare-test.cloudapp.net"
-  REST = "/REST.svc/v1"
-  RESTFILE = REST + "/file"
-  RESTDB = REST + "/db"
+  HOST = "sqlshare-rest-test.cloudapp.net" #"192.168.3.109" #
+  REST = "/REST.svc"
+  RESTFILE = REST + "/v2/file"
+  RESTDB = REST + "/v1/db"
+  CHUNKSIZE = 10*2**20 # 1MB (test)
+  ERROR_NUM = 0
 
   """
 @param host: the DNS URL
@@ -39,9 +41,18 @@ class SQLShare:
     self.password = password
     self.schema = self.get_userinfo()["schema"]
  
+
+  def set_auth_header(self, header):
+    header['Authorization'] = 'ss_user ' + self.username
     
+  def chunksoff(self, f, size):
+    print os.path.getsize(f.name)
+    lines = f.readlines(size)    
+    while lines:
+        yield "".join(lines)	 	  
+        lines = f.readlines(size)	  
   
- 
+	  
   """
 Upload a datasheet into Sql 
 @param tablename: the tablename that should be created on this datafile.  Defaults to filename.
@@ -68,6 +79,32 @@ Upload a datasheet into Sql
     res = h.getresponse()
     resp_msg = res.read()
     return resp_msg
+    
+    
+  def post_file_chunk(self, filepath, dataset_name, chunk, force_append, force_column_headers):
+    tableid = dataset_name or os.path.basename(filename)
+    fileobj = file(filepath)
+    fields = []
+    content_type, body = _encode_multipart_formdata_via_chunks(tableid, chunk)
+
+    h = httplib.HTTPSConnection(self.HOST)
+    headers = {
+      'User-Agent': 'python_multipart_caller',
+      'Content-Type': content_type,      
+    }    
+    self.set_auth_header(headers)
+
+    selector = self.RESTFILE + '?dataset=' + urllib.quote(dataset_name)
+    if force_append != None:
+        selector += '&force_append=%s' % force_append
+    if force_column_headers != None:
+        selector += '&force_column_headers=%s' % force_column_headers
+    
+    h.request('POST', selector, body, headers)
+
+    res = h.getresponse()
+    resp_msg = res.read()
+    return resp_msg
 
   """
 Upload multiple files to sqlshare.  Assumes all files have the same format.
@@ -83,47 +120,62 @@ Upload multiple files to sqlshare.  Assumes all files have the same format.
       tablenames = fnames
     pairs = [(fn, t or os.path.basename(fn)) for (fn, t) in zip(fnames,tablenames)]
     # get user info; we need the schema name
-    info = self.get_userinfo()
-    schema = info["schema"]
 
     for fn,tn in pairs:
-      yield self.uploadone(fn,tn,schema)
+      yield self.uploadone(fn,tn)
 
-  def uploadone(self, fn, tn, schema):
+  def uploadone(self, fn, dataset_name):
+    f = file(fn)
+    first_chunk = True
+    for chunk in self.chunksoff(f, self.CHUNKSIZE):        
+        print 'processing chunk.. %s' % chunk.count('\n')
+        if first_chunk:
+          self.upload_chunk(fn, dataset_name, chunk)
+        else:
+          self.upload_chunk(fn, dataset_name, chunk, True, False)
+        first_chunk = False           
+
+    print "finished %s" % dataset_name
+    return dataset_name
+  
+  def upload_chunk(self, fn, dataset_name, chunk, force_append=None, force_column_headers=None):
       print "pushing %s..." % fn
       # step 1: push file
-      quotedtableid = self.post_file(fn, tn) 
-      datasetname = json.loads(quotedtableid)
-      #datasetname = 'xauthors.csv'
-      tablename = "table_%s" % datasetname
+      jsonuploadid = self.post_file_chunk(fn, dataset_name, chunk, force_append, force_column_headers) 
+      uploadid = json.loads(jsonuploadid)
    
-      print "parsing %s..." % datasetname
-      # step 2: get parse information
-      parser = self.get_parser(datasetname)
-       
-      # step 2.5: optionally change parameters
-      parser['table']['name'] = tablename
-      print parser#['table']['name'] = tablename
+      print "parsing %s..." % uploadid
+      # step 2: get parse information       
+      self.poll_selector('%s/v2/file/%s' % (self.REST, uploadid))
+      
+        
 
-      print "putting %s..." % tablename
-      # step 3: parse and insert to SQL
-      # polls to verify success
-      self.put_table(datasetname, parser)
+  def poll_selector(self, selector):    
+    while True:        
+        h = httplib.HTTPSConnection(self.HOST)
+        headers = {}
+        self.set_auth_header(headers)    
+        h.request('GET', selector, '', headers)
+        res = h.getresponse()
+        if res.status == 200:
+            return res.read()
+        if res.status == 202:
+            time.sleep(0.5)
+            continue
+        else: 
+            raise SQLShareError("code: %s : %s" % (res.status, res.read()))
+
+  def write_error_out(self, chunk):
+    f = open("error_set_%s" % self.ERROR_NUM, 'w')
+    f.write(chunk)
+    f.close()
   
-      print "saving query %s..." % datasetname
-      # step 4: create view
-      q = "SELECT * FROM [%s]" % tablename
-      self.save_query(q, schema, tn, "Uploaded using %s from '%s'" % (__file__,fn))
-
-      print "finished %s" % datasetname
-      return datasetname
-
   def get_userinfo(self):
     h = httplib.HTTPSConnection(self.HOST)
     headers = {
         'Authorization': 'ss_user ' + self.username
     }
-    selector = '%s/user/%s' % (self.REST, self.username)
+    selector = '%s/v1/user/%s' % (self.REST, self.username)
     h.request('GET', selector, '', headers)
     res = h.getresponse()
     if res.status > 400:
@@ -142,7 +194,36 @@ Upload multiple files to sqlshare.  Assumes all files have the same format.
     res = h.getresponse()
     return res
 
-  def save_query(self, sql, schema, name, description):
+  """
+ Get a list of all queries that are available to user 
+  """
+  def get_all_queries(self):    
+    h = httplib.HTTPSConnection(self.HOST)    
+    headers = {}
+    self.set_auth_header(headers)
+    selector = "%s/query" % (self.RESTDB)    
+    h.request('GET', selector, '', headers)
+    res = h.getresponse()
+    if res.status == 200: return json.loads(res.read())
+    else: raise SQLShareError("%s: %s" % (res.status, res.read()))	
+    
+  """
+Get meta data about target query, this also includes a cached sampleset of first 200 rows of data
+  """
+  def get_query(self, schema, query_name):
+    h = httplib.HTTPSConnection(self.HOST)    
+    headers = {}
+    self.set_auth_header(headers)
+    selector = "%s/query/%s/%s" % (self.RESTDB, urllib.quote(schema), urllib.quote(query_name))    
+    h.request('GET', selector, '', headers)
+    res = h.getresponse()
+    if res.status == 200: return json.loads(res.read())
+    else: raise SQLShareError("%s: %s" % (res.status, res.read()))	       
+
+  """
+Save a query
+  """    
+  def save_query(self, sql, name, description, is_public=False):
     h = httplib.HTTPSConnection(self.HOST)
     headers = {
         'Content-Type': 'application/json',
@@ -151,51 +232,91 @@ Upload multiple files to sqlshare.  Assumes all files have the same format.
     }
 
     queryobj = {
-      "description":"description",
+      "description":description,
       "sql_code":sql,
-	    "is_public": False
+	    "is_public": is_public
     }
 
-    selector = "%s/query/%s/%s" % (self.RESTDB, schema, urllib.quote(name)) 
+    selector = "%s/query/%s/%s" % (self.RESTDB, urllib.quote(self.schema), urllib.quote(name)) 
     h.request('PUT', selector, json.dumps(queryobj), headers)
     res = h.getresponse()
     print res
     if res.status == 200: return 'modified'
     elif res.status == 201: return 'created' 
     else: raise SQLShareError("%s: %s" % (res.status, res.read()))
+    
+  def delete_query(self, query_name):
+    h = httplib.HTTPSConnection(self.HOST)    
+    headers = {}
+    self.set_auth_header(headers)
+    selector = "%s/query/%s/%s" % (self.RESTDB, urllib.quote(self.schema), urllib.quote(query_name))    
+    h.request('DELETE', selector, '', headers)       
 
+  def download_sql_result(self, sql):
+    h = httplib.HTTPSConnection(self.HOST)       
+    headers = {}
+    self.set_auth_header(headers)
+    selector = "%s/file?sql=%s" % (self.RESTDB, urllib.quote(sql))    
+    h.request('GET', selector, '', headers)
+    res = h.getresponse()
+    if res.status == 200: return res.read()
+    else: raise SQLShareError("%s: %s" % (res.status, res.read()))	    
+    
+    
+  def execute_sql(self, sql):
+    h = httplib.HTTPSConnection(self.HOST)        
+    headers = {}
+    self.set_auth_header(headers)
+    selector = "%s?sql=%s" % (self.RESTDB, urllib.quote(sql))    
+    h.request('GET', selector, '', headers)
+    res = h.getresponse()
+    if res.status == 202: #accepted
+        location = res.getheader('Location')
+        return json.loads(self.poll_execute_sql(location))
+    else:
+        raise SqlExecuteError("code: %s : %s" % (res.status, res.read()))
+    
+        
   def get_parser(self, tableid):
     resp = SQLShareUploadResponse(self.tableop(tableid, 'parser'))
     while (not resp.done()):
       if resp.failed():
         raise ValueError("%s: %s" % resp.error)
       resp = SQLShareUploadResponse(self.tableop(tableid, 'parser'))
-    return resp.parser
-
+    return resp.parser    
+    
+# for the most bizzare reason, having the check table function inside of the put_table1 method
+# would freeze upon SSL handshake every single time. But it works when its not being called from
+# within that method
   def put_table(self, filename, parser):
+    self.put_table1(filename, parser)
+    success = self.check_table(filename)
+    while not success:
+      time.sleep(0.5)
+      success = self.check_table(filename)
+    return True
+      
+  def put_table1(self, filename, parser):
+   # httplib.HTTPSConnection.debuglevel = 5
     h = httplib.HTTPSConnection(self.HOST)
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'ss_user ' + self.username,
         'Accept': 'application/json'
     }
+    #print parser
+    self.set_auth_header(headers)
     selector = "%s/%s/table" % (self.RESTFILE, urllib.quote(filename))
-    print selector
-    h.request('PUT', selector, json.dumps(parser), headers)
+    h.request('PUT', selector, json.dumps(parser), headers)    
     res = h.getresponse()
+    time.sleep(0.3)    
     if res.status != 200:
-      raise SQLShareUploadError("%s: %s" % (res.status, res.read()))
-
-    success = self.check_table(filename)
-    while not success:
-      success = self.check_table(filename)
-    return True  
+      raise SQLShareUploadError("%s: %s" % (res.status, res.read()))     
 
   def check_table(self, filename):
+    #httplib.HTTPSConnection.debuglevel = 5
     h = httplib.HTTPSConnection(self.HOST)
-    headers = {
-        'Authorization': 'ss_user ' + self.username,
-    }
+    headers = {}
+    self.set_auth_header(headers)
     selector = "%s/%s/table" % (self.RESTFILE, urllib.quote(filename))
     h.request('GET', selector, '', headers)
     res = h.getresponse()
@@ -235,9 +356,30 @@ class SQLShareUploadResponse:
     return self.code >= self.ERROR
 
 
+def _encode_multipart_formdata_via_chunks(filename, chunk):
+
+    BOUNDARY = '----------ThIs_Is_tHe_bouNdaRY_$'
+    CRLF = '\r\n'
+    L = []            
+    file_size = len(chunk)    
+    contenttype = mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+    L.append('--%s' % BOUNDARY)    
+    # L.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key,filename))
+    L.append('Content-Disposition: form-data; name="file1"; filename="%s"' % filename)
+    L.append('Content-Type: %s' % contenttype)    
+    L.append('\r\n' + chunk)
+    L.append('--' + BOUNDARY + '--')
+    L.append('')
+    body = CRLF.join(L)
+    content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
+    return content_type, body
+
+    
     """
 Utility for formatting form data
 @return: (content_type, body) ready for httplib.HTTP instance
+
+DO NOT ERASE, USED AS MULTIPART REFERENCE
     """
 def _encode_multipart_formdata(fields, files):
 
--- tools/singleupload.py
+++ tools/singleupload.py
@@ -4,6 +4,8 @@ Upload multiple files to sqlshare
 
 import sys
 import sqlshare
+import time
+import os
 
 
 """print usage"""
@@ -21,15 +23,16 @@ def main():
   else:
     username, password = sys.argv[1:3]
     filename = sys.argv[3]
-    if len(sys.argv) > 3: 
+    print filename
+    if len(sys.argv) > 4: 
       datasetname = sys.argv[4]
     else: 
-      datasetname = filename
+      datasetname = os.path.basename(filename)    
  
     conn = sqlshare.SQLShare(username,password)
-    info = conn.get_userinfo()
-    schema = info["schema"]
-    conn.uploadone(filename, datasetname, schema)
+    conn.uploadone(filename, datasetname)
+    #x = {u'table': {u'description': None, u'row_count': 9999, u'is_public': False, u'schema': u'gbc3', u'options': {u'on_name_conflict': u'overwrite', u'on_name_conflict_type_mismatch': u'fail'}, u'columns': [{u'max_length': 20, u'type': 3, u'name': u'database'}, {u'max_length': 7, u'type': 3, u'name': u'gene'}, {u'max_length': -1, u'type': 2, u'name': u'size'}, {u'max_length': 7, u'type': 3, u'name': u'site'}, {u'max_length': 14, u'type': 3, u'name': u'read'}, {u'max_length': 8, u'type': 3, u'name': u'orf_coordinate'}, {u'max_length': 24, u'type': 3, u'name': u'attempted_classification'}, {u'max_length': 24, u'type': 3, u'name': u'actual_classification'}, {u'max_length': 6, u'type': 3, u'name': u'tax_id'}, {u'max_length': -1, u'type': 1, u'name': u'pp'}], u'name': u'table_test_append_1'}, u'delimiter': u',', u'first_row_line_num': 0, u'has_column_headers': True}
+    #conn.put_table('test_append_1', x)    
 
 if __name__ == '__main__':
   main()
