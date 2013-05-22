de33305f5587d8fc99d63ffdfab6b426214ef9bb
Bill Howe <billhowe@cs.washington.edu>
2013-05-22 18:09:02 +0000

    Use optionparser; standardize interface
    git-svn-id: https://subversion.assembla.com/svn/sqlshare/pythonclient@822 393f18f5-db89-43d5-bd78-748e0f63f621

--- tools/singleupload.py
+++ tools/singleupload.py
@@ -6,33 +6,37 @@ import sys
 import sqlshare
 import time
 import os
+from optparse import OptionParser
 
 
 """print usage"""
 def usage():
-  cmd = """python %s <username> <password> <file1> <tablename>""" % __file__
+  cmd = """python %s <file1> [<tablename>] [<username> <password>]""" % __file__
   exmp = """
 Example:
-python %s armbrustlab <password> 
+python %s myfile.csv myfile
 """ % __file__
   return cmd + exmp
 
 def main():
-  if len(sys.argv) < 4:
-    print usage()
+  parser = OptionParser(usage="usage: %prog [options] <filename>")
+  parser.add_option('-u', '--user', dest='username', help='SQLshare user name')
+  parser.add_option('-p', '--password', dest='password', help='SQLshare password')
+  parser.add_option('-d', '--datasetname', dest='datasetname', help='Dataset name (defaults to filename if not supplied')
+
+  (options, args) = parser.parse_args()
+
+  if not args:
+    parser.error('no input datafile')
+
+  conn = sqlshare.SQLShare(options.username,options.password)
+
+  if not options.datasetname:
+    datasetname = args[0]
   else:
-    username, password = sys.argv[1:3]
-    filename = sys.argv[3]
-    print filename
-    if len(sys.argv) > 4: 
-      datasetname = sys.argv[4]
-    else: 
-      datasetname = os.path.basename(filename)    
- 
-    conn = sqlshare.SQLShare(username,password)
-    conn.uploadone(filename, datasetname)
-    #x = {u'table': {u'description': None, u'row_count': 9999, u'is_public': False, u'schema': u'gbc3', u'options': {u'on_name_conflict': u'overwrite', u'on_name_conflict_type_mismatch': u'fail'}, u'columns': [{u'max_length': 20, u'type': 3, u'name': u'database'}, {u'max_length': 7, u'type': 3, u'name': u'gene'}, {u'max_length': -1, u'type': 2, u'name': u'size'}, {u'max_length': 7, u'type': 3, u'name': u'site'}, {u'max_length': 14, u'type': 3, u'name': u'read'}, {u'max_length': 8, u'type': 3, u'name': u'orf_coordinate'}, {u'max_length': 24, u'type': 3, u'name': u'attempted_classification'}, {u'max_length': 24, u'type': 3, u'name': u'actual_classification'}, {u'max_length': 6, u'type': 3, u'name': u'tax_id'}, {u'max_length': -1, u'type': 1, u'name': u'pp'}], u'name': u'table_test_append_1'}, u'delimiter': u',', u'first_row_line_num': 0, u'has_column_headers': True}
-    #conn.put_table('test_append_1', x)    
+    datasetname = options.datasetname
+
+  conn.uploadone(args[0], datasetname)
 
 if __name__ == '__main__':
   main()
