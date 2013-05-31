6bb861f3684689d8e4f0a75afb64fc832b3667d6
Daniel Halperin <dhalperi@cs.washington.edu>
2013-05-31 09:38:00 -0700

    add @dhalperi to the setup.py authors list.
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='SQLShare',
   version='0.1a',
   description='Python API to the SQLShare Database Service',
-  author='Bill Howe, Garret Cole',
+  author='Bill Howe, Garret Cole, Daniel Halperin',
   author_email='billhowe@cs.washington.edu',
   url='http://escience.washington.edu/sqlshare',
   packages=find_packages()
