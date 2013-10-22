3fc8782310c4ee3d3d19199d12d9fc0689b97588
Daniel Halperin <dhalperi@cs.washington.edu>
2013-10-22 00:53:01 -0700

    tools/permissions.py: basic functionality for public/private datasets
    Close #11.
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- tools/permissions.py
+++ tools/permissions.py
@@ -7,7 +7,7 @@ from optparse import OptionParser
 
 
 def main():
-    parser = OptionParser(usage="usage: %prog [options] (add|remove|print) <user1> <user2> ... <userN>")
+    parser = OptionParser(usage="usage: %prog [options] (add|remove|print|public|private) <user1> <user2> ... <userN>")
     parser.add_option('-u', '--user', dest='username', help='SQLshare user name')
     parser.add_option('-p', '--password', dest='password', help='SQLshare password')
     parser.add_option('-t', '--table', dest='tables', help='SQLshare dataset', action='append')
@@ -22,10 +22,10 @@ def main():
 
     cmd = args[0]
 
-    if cmd not in ( 'add', 'print', 'remove'):
+    if cmd not in ('add', 'print', 'public', 'private', 'remove'):
         parser.error('unknown command: '+cmd)
 
-    if cmd != 'print':
+    if cmd == 'add' or cmd == 'remove':
         users = args[1:]
         if not users:
             parser.error('user list is required for %s command' % cmd)
@@ -38,14 +38,19 @@ def main():
         perms[table] = conn.get_permissions(table)
         if cmd == 'print':
             print perms[table]
-        elif cmd == 'add':
+            continue
+        if cmd == 'add':
             viewers = list(set(perms[table]['authorized_viewers']) | users)
             perms[table]['authorized_viewers'] = viewers
-            conn.set_permissions(table, **perms[table])
         elif cmd == 'remove':
             viewers = list(set(perms[table]['authorized_viewers']) - users)
             perms[table]['authorized_viewers'] = viewers
             conn.set_permissions(table, **perms[table])
+        elif cmd == 'public':
+            perms[table]['is_public'] = True
+        elif cmd == 'private':
+            perms[table]['is_public'] = False
+        conn.set_permissions(table, **perms[table])
 
 if __name__ == '__main__':
     main()
